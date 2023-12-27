e6b2ca7df4259f01ed94e5ca59144489d2a26b5b
mtekinn <tekinmdev@gmail.com>
2023-12-27 22:31:58 +0300

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Mehmet Tekin on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
